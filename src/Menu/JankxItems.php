c6234152bcbfe4e79de472bd5e16526383141230
Puleeno Nguyen <puleeno@gmail.com>
2020-09-19 21:05:42 +0700

    Support added subtitle to menu item

--- src/Menu/JankxItems.php
+++ src/Menu/JankxItems.php
@@ -1,14 +1,31 @@
 <?php
 namespace Jankx\SiteLayout\Menu;
 
+use Jankx\SiteLayout\Menu\NavItemRenderer;
+
 class JankxItems
 {
     protected static $jankxNavItems;
 
+    protected $renderer;
+
+    public function __construct()
+    {
+        $this->renderer = new NavItemRenderer();
+    }
+
     public function register()
     {
+        add_filter('manage_nav-menus_columns', array($this, 'add_item_subtitle'), 15);
+        add_action('wp_nav_menu_item_custom_fields', array($this, 'add_custom_subtitle_field'), 10, 5);
+        add_action('save_post', array($this, 'save_subtile_metadata'), 10, 2);
+
         add_action('admin_head-nav-menus.php', array( $this, 'add_menu_meta_boxes' ));
         add_filter('wp_setup_nav_menu_item', array($this, 'setup_jankx_items'));
+
+        add_filter('wp_nav_menu_objects', array($this->renderer, 'resetWalkerSupportHookStartEl'));
+        add_filter('walker_nav_menu_start_el', array($this->renderer, 'renderMenuItem'), 10, 4);
+        add_filter('nav_menu_item_title', array($this->renderer, 'renderMenuItemSubtitle'), 10, 4);
     }
 
     public function add_menu_meta_boxes()
@@ -22,15 +39,19 @@ class JankxItems
         );
     }
 
-    protected function get_nav_items()
+    public static function get_nav_items()
     {
         if (!is_null(static::$jankxNavItems)) {
             return static::$jankxNavItems;
         }
 
         static::$jankxNavItems = array(
-            'jankx_logo' => __('Site Logo', 'jankx'),
-            'jankx_search_form' => __('Search Form', 'jankx')
+            'jankx-logo' => __('Site Logo', 'jankx'),
+            'jankx-search-form' => __('Search Form', 'jankx')
+        );
+        static::$jankxNavItems = apply_filters(
+            'jankx_site_layout_menu_items',
+            static::$jankxNavItems
         );
 
         return static::$jankxNavItems;
@@ -39,7 +60,7 @@ class JankxItems
     protected function create_menu_nav_item($key)
     {
         $title = '';
-        if ($key === 'jankx_logo') {
+        if ($key === 'jankx-logo') {
             $title = get_bloginfo('name');
         }
 
@@ -50,7 +71,7 @@ class JankxItems
             'classes' => null
         ));
 
-        return apply_filters("jankx_site_layout_{$key}_nav_item", $item, $key);
+        return apply_filters("jankx_site_layout_{$key}_menu_item", $item, $key);
     }
 
     protected function render_menu_item_hidden_input($index, $item)
@@ -65,7 +86,7 @@ class JankxItems
                 <input
                     type="hidden"
                     class="menu-item-type"
-                    name="menu-item[<?php echo esc_attr($index); ?>][menu-item-<?php echo $type; ?>]"
+                    name="menu-item[<?php echo esc_attr($index); ?>][menu-item-<?php echo $type; ?>]<?php  ?>"
                     value="<?php echo $value; ?>"
                 />
             <?php endif; ?>
@@ -75,7 +96,7 @@ class JankxItems
 
     public function nav_menu_links()
     {
-        $items = $this->get_nav_items();
+        $items = static::get_nav_items();
         ?>
         <div id="posttype-jankx-nav-items" class="posttypediv">
             <div id="tabs-panel-jankx-nav-items" class="tabs-panel tabs-panel-active">
@@ -104,9 +125,6 @@ class JankxItems
                 </ul>
             </div>
             <p class="button-controls">
-                <span class="list-controls">
-                    <a href="<?php echo esc_url(admin_url('nav-menus.php?page-tab=all&selectall=1#posttype-jankx-nav-items')); ?>" class="select-all"><?php esc_html_e('Select all', 'jankx'); ?></a>
-                </span>
                 <span class="add-to-menu">
                     <button
                         type="submit"
@@ -126,12 +144,58 @@ class JankxItems
 
     public function setup_jankx_items($menu_item)
     {
-        $items = $this->get_nav_items();
+        $items = static::get_nav_items();
 
         if (isset($items[$menu_item->type])) {
             $menu_item->type_label = sprintf('Jankx %s', $items[$menu_item->type]);
         }
 
         return $menu_item;
+    }
+
+    public function add_item_subtitle($columns)
+    {
+        $columns = array_merge($columns, array(
+            'subtitle' => __('Jankx Subtitle', 'jankx'),
+        ));
+        return $columns;
+    }
+
+    public function add_custom_subtitle_field($item_id, $item, $depth, $args, $id)
+    {
+        $subtitle = get_post_meta($item_id, '_jankx_menu_item_subtitle', true);
+        ?>
+        <p class="jankx-field description description-wide">
+            <label for="edit-menu-item-subtitle-<?php echo $item_id; ?>">
+                <?php _e('Jankx Subtitle', 'jankx'); ?><br>
+                <input
+                    type="text"
+                    id="edit-menu-item-subtitle-<?php echo $item_id; ?>"
+                    class="widefat code edit-menu-item-subtitle"
+                    name="menu-item-subtitle[<?php echo $item->ID; ?>]"
+                    value="<?php echo trim($subtitle); ?>"
+                />
+                <span class="description">
+                    <?php _e('The subtitle will be displayed in below item text', 'jankx'); ?>
+                </span>
+            </label>
+        </p>
+        <?php
+    }
+
+    public function save_subtile_metadata()
+    {
+        if (empty($_POST['menu-item-subtitle'])) {
+            return;
+        }
+        $subtitles = array_filter($_POST['menu-item-subtitle']);
+
+        foreach ($subtitles as $post_ID => $subtitle) {
+            $menuItem = get_post($post_ID);
+            if (!$menuItem || $menuItem->post_type !== 'nav_menu_item') {
+                continue;
+            }
+            update_post_meta($post_ID, '_jankx_menu_item_subtitle', $subtitle);
+        }
     }
 }
---
+++ src/Menu/NavItemRenderer.php
@@ -0,0 +1,75 @@
+<?php
+namespace Jankx\SiteLayout\Menu;
+
+use Jankx\Option\Option;
+use Jankx\SiteLayout\Menu\JankxItems;
+
+class NavItemRenderer
+{
+    protected $hook_walker_nav_menu_start_el_is_called;
+
+    public function resetWalkerSupportHookStartEl($sorted_menu_items)
+    {
+        $this->hook_walker_nav_menu_start_el_is_called = false;
+
+        // Does not do anythin and return $sorted_menu_items
+        return $sorted_menu_items;
+    }
+
+    public function getJankxLogo($item, $depth, $args)
+    {
+        $logoType = Option::get('logo_type', 'image');
+        return jankx_component('logo', array(
+            'type' => $logoType,
+            'text' => $item->post_title,
+            'image_url' => Option::get('logo_image_url'),
+        ));
+    }
+
+    protected function getContent($item_output, $item, $depth, $args)
+    {
+        $method = sprintf("get%s", preg_replace_callback(array('/^([a-z])/', '/[-_]([a-z])/'), function ($matches) {
+            if (isset($matches[1])) {
+                return strtoupper($matches[1]);
+            }
+        }, $item->type));
+        $callable = apply_filters('jankx_site_layout_nav_item_callback', array($this, $method), $item, $depth, $args);
+
+        if (!is_callable($callable)) {
+            return $item_output;
+        }
+
+        return call_user_func($callable, $item, $depth, $args);
+    }
+
+    public function renderMenuItem($item_output, $item, $depth, $args)
+    {
+        // Create the flag to
+        if (!$this->hook_walker_nav_menu_start_el_is_called) {
+            $this->hook_walker_nav_menu_start_el_is_called = true;
+        }
+        $jankxItems = JankxItems::get_nav_items();
+
+        if (isset($jankxItems[$item->type])) {
+            $pre = apply_filters("jankx_site_layout_nav_item_{$item->type}", null, $item_output, $item, $depth, $args);
+            if ($pre) {
+                return $pre;
+            }
+            $content = $this->getContent($item_output, $item, $depth, $args);
+            if ($content) {
+                $item_output = $content;
+            }
+        }
+
+        return $item_output;
+    }
+
+    public function renderMenuItemSubtitle($title, $item, $args, $depth)
+    {
+        $subtitle = get_post_meta($item->ID, '_jankx_menu_item_subtitle', true);
+        if (!$subtitle) {
+            return $title;
+        }
+        return sprintf('%s<span class="jankx-subtitle menu-item-subtitle">%s</span>', $title, $subtitle);
+    }
+}

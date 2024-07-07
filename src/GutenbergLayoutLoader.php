7bacd481fcb26eeae5f00054a7d167fcc0f172ae
Puleeno Nguyen <puleeno@gmail.com>
2024-07-07 21:59:26 +0700

    Remove custom

--- src/GutenbergLayoutLoader.php
+++ src/GutenbergLayoutLoader.php
@@ -7,15 +7,4 @@ class GutenbergLayoutLoader extends LayoutLoader
     protected $layout;
     protected $engine;
     protected $fullContent = false;
-
-
-    /**
-     * Load site layout via template engine
-     *
-     * @param \Jankx\TemplateEngine\Engine $engine The template engine use in theme
-     */
-    public function load()
-    {
-        do_action('jankx/template/site/layout', $this);
-    }
 }
